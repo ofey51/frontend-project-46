acab25a3a5ba5a09f11b2bf6e2f8b3f5c783a1bd
ofey51 <domenuk10@gmail.com>
2022-11-10 23:48:02 +0300

    add genDiff

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 import { program } from 'commander';
+import genDiff from '../src/index.js';
 
 program
   .name('gendiff')
@@ -7,5 +8,8 @@ program
   .version('0.2.0')
   .option('-f, --format <type>', 'output format')
   .arguments('<filepath1> <filepath2>')
+  .action((path1, path2) => {
+    console.log(genDiff(path1, path2));
+  })
 
 program.parse();
--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
-import genDiff from '@hexlet/code';
+import { readFileSync } from 'node:fs';
+import _ from 'lodash';
+import path from 'node:path'
 
-console.log(genDiff('file1', 'file2'));
+const genDiff = (path1, path2) => {
+    const object1 = JSON.parse(readFileSync(path.resolve(path1)));
+    const object2 = JSON.parse(readFileSync(path.resolve(path2)));
+    const key1 = _.keys(object1);
+    const key2 = _.keys(object2);
+    const uniqKeys = _.uniq([...key1, ...key2]).sort();
+    let result = '\n{';
+    for (const key of uniqKeys) {
+        if (key1.includes(key) && key2.includes(key)) {
+            if (object1[key] === object2[key]) {
+                result = `${result}\n    ${key}: ${object1[key]}`
+            } else {
+                result = `${result}\n  - ${key}: ${object1[key]}\n  + ${key}: ${object2[key]}`
+            }
+        } else if (key1.includes(key)) {
+            result = `${result}\n  - ${key}: ${object1[key]}`;
+        } else {
+            result = `${result}\n  + ${key}: ${object2[key]}`;
+        }
+    }
+    return result + '\n}'
+};
+
+export default genDiff;
