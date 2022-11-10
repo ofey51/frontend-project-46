e8e06b8eada0b2c4fad01af2c34122064f90a66f
ofey51 <domenuk10@gmail.com>
2022-11-11 00:04:33 +0300

    refact index.js

--- src/index.js
+++ src/index.js
@@ -5,18 +5,16 @@ import path from 'node:path'
 const genDiff = (path1, path2) => {
     const object1 = JSON.parse(readFileSync(path.resolve(path1)));
     const object2 = JSON.parse(readFileSync(path.resolve(path2)));
-    const key1 = _.keys(object1);
-    const key2 = _.keys(object2);
-    const uniqKeys = _.uniq([...key1, ...key2]).sort();
+    const uniqKeys = _.sortBy(_.uniq([...Object.keys(object1), ...Object.keys(object2)]));
     let result = '\n{';
     for (const key of uniqKeys) {
-        if (key1.includes(key) && key2.includes(key)) {
+        if (Object.hasOwn(object1, key) && Object.hasOwn(object2, key)) {
             if (object1[key] === object2[key]) {
                 result = `${result}\n    ${key}: ${object1[key]}`
             } else {
                 result = `${result}\n  - ${key}: ${object1[key]}\n  + ${key}: ${object2[key]}`
             }
-        } else if (key1.includes(key)) {
+        } else if (Object.hasOwn(object1, key)) {
             result = `${result}\n  - ${key}: ${object1[key]}`;
         } else {
             result = `${result}\n  + ${key}: ${object2[key]}`;
