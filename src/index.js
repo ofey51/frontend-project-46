f11da8db1f3dfac5b33c171d37e5a009a60ce8e2
ofey51 <domenuk10@gmail.com>
2022-11-11 11:33:47 +0300

    getting rid of mutations

--- src/index.js
+++ src/index.js
@@ -6,21 +6,19 @@ const genDiff = (path1, path2) => {
     const object1 = JSON.parse(readFileSync(path.resolve(path1)));
     const object2 = JSON.parse(readFileSync(path.resolve(path2)));
     const uniqKeys = _.sortBy(_.uniq([...Object.keys(object1), ...Object.keys(object2)]));
-    let result = '\n{';
-    for (const key of uniqKeys) {
+    return '\n{' + uniqKeys.map((key) => {
         if (Object.hasOwn(object1, key) && Object.hasOwn(object2, key)) {
             if (object1[key] === object2[key]) {
-                result = `${result}\n    ${key}: ${object1[key]}`
+                return `\n    ${key}: ${object1[key]}`;
             } else {
-                result = `${result}\n  - ${key}: ${object1[key]}\n  + ${key}: ${object2[key]}`
+                return `\n  - ${key}: ${object1[key]}\n  + ${key}: ${object2[key]}`;
             }
         } else if (Object.hasOwn(object1, key)) {
-            result = `${result}\n  - ${key}: ${object1[key]}`;
+            return `\n  - ${key}: ${object1[key]}`;
         } else {
-            result = `${result}\n  + ${key}: ${object2[key]}`;
+            return `\n  + ${key}: ${object2[key]}`;
         }
-    }
-    return result + '\n}'
+    }) + '\n}';
 };
 
 export default genDiff;
