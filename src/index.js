ea9dea85d7d01e3fef68bb5ce114902edf70ddc3
ofey51 <domenuk10@gmail.com>
2022-11-11 11:49:01 +0300

    refactor index.js

--- src/index.js
+++ src/index.js
@@ -6,19 +6,18 @@ const genDiff = (path1, path2) => {
     const object1 = JSON.parse(readFileSync(path.resolve(path1)));
     const object2 = JSON.parse(readFileSync(path.resolve(path2)));
     const uniqKeys = _.sortBy(_.uniq([...Object.keys(object1), ...Object.keys(object2)]));
-    return '\n{' + uniqKeys.map((key) => {
+    const diff = uniqKeys.map((key) => {
+        const property = `${key}: ${object1[key]}`;
         if (Object.hasOwn(object1, key) && Object.hasOwn(object2, key)) {
-            if (object1[key] === object2[key]) {
-                return `\n    ${key}: ${object1[key]}`;
-            } else {
-                return `\n  - ${key}: ${object1[key]}\n  + ${key}: ${object2[key]}`;
-            }
+            return object1[key] === object2[key] ? `\n    ${property}` : `\n  - ${property}\n  + ${property}`;
         } else if (Object.hasOwn(object1, key)) {
-            return `\n  - ${key}: ${object1[key]}`;
+            return `\n  - ${property}`;
         } else {
-            return `\n  + ${key}: ${object2[key]}`;
+            return `\n  + ${property}`;
         }
-    }) + '\n}';
+    });
+    
+    return `\n{${diff}\n}`;
 };
 
 export default genDiff;
