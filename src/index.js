c3c5fec54d2312666b1d0aa2c131db44408b5f3b
ofey_debian23 <domenuk10@gmail.com>
2024-11-01 22:33:43 +0300

    bla-bla

--- src/index.js
+++ src/index.js
@@ -2,23 +2,35 @@ import { readFileSync } from 'node:fs';
 import _ from 'lodash';
 import path from 'node:path';
 
+function parseFile(filePath) {
+  return JSON.parse(readFileSync(path.resolve(filePath)));
+}
+
+const getDiffLine = (key, value1, value2) => {
+  if (value1 === value2) {
+    return `\n    ${key}: ${value1}`;
+  }
+  return `\n  - ${key}: ${value1}\n  + ${key}: ${value2}`;
+};
+
+const getKeyDiff = (key, obj1, obj2) => {
+  const value1 = obj1[key];
+  const value2 = obj2[key];
+
+  if (key in obj1 && key in obj2) {
+    return getDiffLine(key, value1, value2);
+  }
+  return key in obj1 ? `\n  - ${key}: ${value1}` : `\n  + ${key}: ${value2}`;
+};
+
 const genDiff = (path1, path2) => {
-  const obj1 = JSON.parse(readFileSync(path.resolve(path1)));
-  const obj2 = JSON.parse(readFileSync(path.resolve(path2)));
+  const obj1 = parseFile(path1);
+  const obj2 = parseFile(path2);
 
-  const unsortedUniqKeys = _.uniq([...Object.keys(obj1), ...Object.keys(obj2)]);
-  const uniqKeys = _.sortBy(unsortedUniqKeys);
-  const diff = uniqKeys.map((key) => {
-    const value1 = obj1[key];
-    const value2 = obj2[key];
-
-    if (Object.hasOwn(obj1, key) && Object.hasOwn(obj2, key)) {
-      return value1 === value2
-        ? `\n    ${key}: ${value1}`
-        : `\n  - ${key}: ${value1}\n  + ${key}: ${value2}`;
-    }
-    return key in obj1 ? `\n  - ${key}: ${value1}` : `\n  + ${key}: ${value2}`;
-  });
+  const uniqKeys = _.sortBy(
+    _.uniq([...Object.keys(obj1), ...Object.keys(obj2)]),
+  );
+  const diff = uniqKeys.map((key) => getKeyDiff(key, obj1, obj2));
 
   return `\n{${diff.join('')}\n}`;
 };
